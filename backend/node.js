ce4cbd4230535542c3395a77da8aaecefa8ff623
David Albertson <albertson.d@gmail.com>
2012-09-12 00:25:37 -0400

    Fix Parsing domain URL

--- backend/node.js
+++ backend/node.js
@@ -6,41 +6,42 @@ var Server = require('/home/pag/public_html/node_modules/mongodb').Server;
 var $count = 0;
 
 function myGetTime() {
-        return new Date(hours, minutes, seconds, milliseconds);
+    return new Date();
 }
 
 var server = http.createServer(function (request, response) {
-	// URL formatting and setting DB context
-	// --Is there a more efficent method for formatting?
-	domainRepPeriod = request.headers.origin.replace(/\./g,'-');
-	domainFormatted = domainRepPeriod.replace('http://','');
-	db = new Db(domainFormatted, new Server('localhost', 27017, {}), {});
+    // URL formatting and setting DB context
+    // --Is there a more efficent method for formatting?
+    urlParsing = url.parse(request.headers.origin);
 
-	// Variable
-	ip_address = request.connection.remoteAddress;
-	domain = request.connection.serverName;
+    domainFormatted = urlParsing.hostname.replace(/\./g,'-');
+    db = new Db(domainFormatted, new Server('localhost', 27017, {}), {});
+
+    // Variable
+    ip_address = request.connection.remoteAddress;
+    domain = request.connection.serverName;
     pathname = url.parse(request.url).query;
-	
-	// Open DB + Insertion
+
+    // Open DB + Insertion
     db.open(function() {
-		db.collection('visits', function(err, collection){
+	db.collection('visits', function(err, collection){
             doc={
                 "origin"  :     request.headers.origin,
                 "ip"  :         ip_address,
                 "url-request" : pathname
             };
 	
-			collection.insert(doc, function(){
-				console.log("Data inserted in DB");
-			});
+	    collection.insert(doc, function(){
+	    	console.log("Data inserted in DB");
+            });
         });
     });
 
-	// Response
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.end("Broccoli v1.0\n");
-	
-	// Console logging
+    // Response
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Broccoli v1.0\n");
+
+    // Console logging
     console.log(" ", $count, " : URL - ", pathname);
     console.log(" ", $count, ' : Host - ', request.headers.origin);
     console.log(" ", $count, ' : Request - ', myGetTime(),'- ',  ip_address, ' from : ', request.headers.referer );
