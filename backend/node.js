1413ea1e409556809039561c5a9caccefdb9f495
David Albertson <albertson.d@gmail.com>
2012-09-11 23:55:41 -0400

    DB test code updated

--- backend/node.js
+++ backend/node.js
@@ -1,50 +1,56 @@
 var http = require('http');
 var url = require('url');
-var mongo = require('/home/pag/public_html/node_modules/mongodb'),
-        db = new mongo.Db('www-dalbertson-com', new mongo.Server('localhost', 27017, {}), {});
-db.open(function() {
-	console.log("DBCreate");
-});
+var Db = require('/home/pag/public_html/node_modules/mongodb').Db;
+var Server = require('/home/pag/public_html/node_modules/mongodb').Server;
 
 var $count = 0;
 
 function myGetTime() {
-    var dd = new Date();
-    var hh = dd.getHours();
-    var mm = dd.getMinutes();
-    var ss = dd.getSeconds();
-	return dd;
+        return new Date(hours, minutes, seconds, milliseconds);
 }
 
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Broccoli v1.0\n");
-  ip_address = request.connection.remoteAddress;
-  domain = request.connection.serverName;
-  
-        // var pathname = url.parse(request.url);
-        var pathname = url.parse(request.url).query;
+	// URL formatting and setting DB context
+	// --Is there a more efficent method for formatting?
+	domainRepPeriod = request.headers.origin.replace(/\./g,'-');
+	domainFormatted = domainRepPeriod.replace('http://','');
+	db = new Db(domainFormatted, new Server('localhost', 27017, {}), {});
 
-	db.collection('visits', function(err, collection){
-                doc={
-                        "origin"  :     request.headers.origin,
-                        "ip"  :     	ip_address,
-                        "url-request" :	pathname
-                };
-
-        collection.insert(doc, function(){
-        console.log("inserted");
+	// Variable
+	ip_address = request.connection.remoteAddress;
+	domain = request.connection.serverName;
+    pathname = url.parse(request.url).query;
+	
+	// Open DB + Insertion
+    db.open(function() {
+		db.collection('visits', function(err, collection){
+            doc={
+                "origin"  :     request.headers.origin,
+                "ip"  :         ip_address,
+                "url-request" : pathname
+            };
+	
+			collection.insert(doc, function(){
+				console.log("Data inserted in DB");
+			});
         });
-        });
+    });
 
-	console.log(" ", $count, " : URL - ", pathname);
-	console.log(" ", $count, ' : Host - ', request.headers.origin);
-	console.log(" ", $count, ' : Request - ', myGetTime().getHours(),':', myGetTime().getMinutes(),':', myGetTime().getSeconds(),'- ',  ip_address, ' from : ', request.headers.referer );
-	console.log("------------");
-	$count++;
-
+	// Response
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.end("Broccoli v1.0\n");
+	
+	// Console logging
+    console.log(" ", $count, " : URL - ", pathname);
+    console.log(" ", $count, ' : Host - ', request.headers.origin);
+    console.log(" ", $count, ' : Request - ', myGetTime(),'- ',  ip_address, ' from : ', request.headers.referer );
+    console.log("------------");
+    $count++;
 });
 
 server.listen(8080);
 
 console.log("Server running...");
+
+
+
