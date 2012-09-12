6aa563ba4a2c94a4447a98d6893e696a0375be94
diastro <albertson.d@gmail.com>
2012-09-12 14:43:00 -0300

    Update backend/broccoli-alpha-client.js
    Fixed some indentation and function name

--- backend/broccoli-alpha-client.js
+++ backend/broccoli-alpha-client.js
@@ -1,28 +1,29 @@
+// Variable declaration for request to main server
 var $pathname = window.location.pathname;
 var $param = window.location.search;
 
 // Load the jquery script if needed
-function loadScript(url, callback) {
+function loadjQuery(url, callback) {
 	if (typeof jQuery == 'undefined') {
 		console.log("jQuery will be loaded");
 		var script = document.createElement("script")
-        script.type = "text/javascript";
+        	script.type = "text/javascript";
 
-        if (script.readyState) { //IE
-                script.onreadystatechange = function () {
-                        if (script.readyState == "loaded" || script.readyState == "complete") {
-                                script.onreadystatechange = null;
-                                callback();
-                        }
-                };
-        } else { //Others
-                script.onload = function () {
-                        callback();
-                };
-        }
-
-        script.src = url;
-        document.getElementsByTagName("head")[0].appendChild(script);
+        	if (script.readyState) { // ie
+	                script.onreadystatechange = function () {
+	                        if (script.readyState == "loaded" || script.readyState == "complete") {
+	                                script.onreadystatechange = null;
+	                                callback();
+	                        }
+	                };
+	        } else { // Chrome - firefox
+	                script.onload = function () {
+	                	callback();
+	                };
+	        }
+	
+	        script.src = url;
+	        document.getElementsByTagName("head")[0].appendChild(script);
 	} else {
 		console.log("jQuery won't be loaded");
 		callback();
@@ -30,13 +31,13 @@ function loadScript(url, callback) {
 }
 
 // Load the script if needed, make jquery ajax request using callback
-loadScript("https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js", function ajax(){
+loadjQuery("https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js", function ajax(){
 	$(window).load(function() {
-			$.ajax({
-					url: 'http://kepler.step.polymtl.ca:8080',
-					type: 'GET',
-					data: $pathname + $param
-			});
+		$.ajax({
+			url: 'http://kepler.step.polymtl.ca:8080',
+			type: 'GET',
+			data: $pathname + $param
+		});
 	});
 });
 
